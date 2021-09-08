40add7dbc71ddd73337f1a7e696e35df25c540a5
tauimonen <tauimonen@gmail.com>
2021-09-08 22:14:22 +0300

    fix nested function

--- w1_tilas.c
+++ w1_tilas.c
@@ -2,24 +2,65 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+int uniq(double arr[], int n)
+    {
+        bool first;
+        int i;
+        int j;
+        int count;
+        first = true;
+
+        for(i = 0; i < n; i++)
+        {
+            count = 0;
+            for(j = 0; j < n; j++)
+            {
+                if (i != j)
+                {
+		            if(arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            if(count == 0)
+            {
+                if (first == true)
+                {
+                    printf("Ainutlaatuiset:");
+                    first = false;
+                }
+                printf(" %f", arr[i]);
+            }
+        }
+        if (first == false)
+        {
+            printf("\n");
+        }
+        return 0;
+    }
+
 int main(int argc, char *argv[])
 {
     double small;
     double large;
     int count;
     int max_count;
-    int index;
+    int index, u_index;
     bool is_first;
     int n;
     int i, j;
     double* most_usual_values;
+    double* unique; 
     is_first = true;
     n = argc - 1;
     most_usual_values = malloc(sizeof(double) * n);
+    unique = malloc(sizeof(double) * n);
     count = 0;
     max_count = 0;
     index = 0;
-
+    u_index = 0;
 
     for (i = 1; i <= n; ++i)
     {
@@ -40,7 +81,7 @@ int main(int argc, char *argv[])
             large = atof(argv[i]);
         }
 
-        for (j = i; j <= n; ++j)
+        for (j = i + 1; j <= n; ++j)
         {
             if (atof(argv[i]) == atof(argv[j]))
             {
@@ -62,18 +103,29 @@ int main(int argc, char *argv[])
             most_usual_values[index] = atof(argv[i]);
             index += 1;
         }
+
         count = 0;
+
     }
+
     printf("Pienin: %f\n", small);
     printf("Suurin: %f\n", large);
-    if (max_count > 1)
+
+    for (u_index = 0; u_index < n; ++u_index)
     {
-        printf("Useimmiten esiintyneet (%d kertaa):", max_count);
+        unique[u_index] = atof(argv[u_index + 1]);
+    }
+
+    uniq(unique, n);
+  
+    if (max_count > 0)
+    {
+        printf("Useimmiten esiintyneet (%d kertaa):", max_count + 1);
         for (i = 0; i <= index - 1; ++i)
         {
             printf(" %f", most_usual_values[i]);
         }
         printf("\n");
     }
-    
+    return 0;
 }
