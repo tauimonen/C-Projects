98a51b7547befb296fa8f3595eb6f88dc2f2b757
tauimonen <tauimonen@gmail.com>
2021-09-10 16:17:36 +0300

    fix error - too small array

--- w1_param.c
+++ w1_param.c
@@ -1,10 +1,15 @@
+/*
+Program prints the comment line parameters as 
+a table with indexes.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void repeat(int c, int times) {
     int i;
-    for (i = 0; i <= times; ++i) {
+    for (i = 0; i < times; ++i) {
         printf("%c", c);
     }
 }
@@ -16,7 +21,7 @@ int main(int argc, char *argv[]) {
     int paramCount;
     int paramWidth;
     int totalWidth;
-    char *strings[30];
+    char *strings[500];
     n = argc - 1;
     maxLen = 0;
     paramWidth = 1;
@@ -35,4 +40,21 @@ int main(int argc, char *argv[]) {
 
     totalWidth = 7 + paramWidth + maxLen; 
     repeat(35, totalWidth);
+    printf("\n");
+
+    for (i = 0; i < n; ++i) {
+        printf("%c %*d | %-*s %c\n", 35, paramWidth, i+1, maxLen, strings[i], 35);
+        if (i < n - 1) {
+            repeat(35, 1);
+            repeat(45, paramWidth + 2);
+            repeat(43, 1);
+            repeat(45, maxLen + 2);
+            repeat(35, 1);
+            printf("\n");
+        }
+    } 
+    repeat(35, totalWidth);
+    printf("\n");
+
+    return 0;
 }
