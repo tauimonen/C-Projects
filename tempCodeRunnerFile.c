200cdccd39d38c96ad4c2baba148933ec159d2f3
tauimonen <tauimonen@gmail.com>
2021-09-20 21:08:09 +0300

    use argv only

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <bool.h>
 
+int main(int argc, char *argv[]) {
+    int len;
+    int i;
+    int sum;
+    bool isFirst;
+    isFirst = true;
+    sum = 0;
+    len = strlen(argv[1]) - 1;
+
+    for (i = len; i >= 0; --i) {
+        if (argv[i] == 1) {
+            if (isFirst) {
+                sum += -(pow(2, i));
+                isFirst = false;
+            } else {
+                sum += -pow(2, i);
+            }
+        }
+    }
+
+    printf("%d", sum);
+}
---
+++ w2_kahd_komp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int sum, i, n, index;
+    char sign;
+    sign = argv[1][0];
+    sum = 0;
+    n = strlen(argv[1]) - 1;
+
+    for (i = 0; i <= n; ++i) {
+        index = n - i;
+        if (argv[1][i] == '1') {
+            if (sign == '1' && i == 0) {
+                sum += -(pow(2, index));
+            } else {
+                sum += pow(2, index);
+            }
+            printf("i: %d, index: %d, sum: %d\n", i, index, sum);
+        } 
+    }
+    printf("%d\n", sum);
+    
+    return 0;
+}
+
