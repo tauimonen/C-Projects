22f173cee5cadcbdac222407ceb840495bdb028e
tauimonen <tauimonen@gmail.com>
2021-09-23 13:39:10 +0300

    make all the functions ready

--- w2_bit.c
+++ w2_bit.c
@@ -1,30 +1,99 @@
+/* 
+All functions print the following four pieces of information for the value obtained 
+as a parameter on the screen:
+Value x.
+Bitmap of x.
+The number of 0-bits contained in the bit representation of x.
+The number of 1-bits contained in the bit representation of x.
+
+Note that the long int type is 64-bit in ANSI C. Otherwise, change the type (long long).
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "w2_bit.h"
-
+#include "bitit.h"
 
 
 void scharBitit(signed char x) {
-    int i, k;
+    int i, k, ones, zeros, bits;
+    ones = 0;
+    zeros = 0;
+    bits = 8;
 
     printf("%d\n", x);
-
-    for (i = 7; i >= 0; i--) {
+    for (i = bits - 1; i >= 0; i--) {
         k = x >> i;
-        if (k & 1)
+        if (k & 1) {
             printf("1");
-        else
+            ones++;
+        }
+        else {
             printf("0");
+            zeros++;
+        }
     }
-
-    printf("\n");
+    printf("\n%d\n%d\n", zeros, ones);
 }
 
+void shortBitit(short int x) {
+    int i, k, ones, zeros, bits;
+    ones = 0;
+    zeros = 0;
+    bits = 16;
 
-int main() {
-    signed char x;
-    x = 2015;
-    scharBitit(x);
+    printf("%d\n", x);
+    for (i = bits - 1; i >= 0; i--) {
+        k = x >> i;
+        if (k & 1) {
+            printf("1");
+            ones++;
+        }
+        else {
+            printf("0");
+            zeros++;
+        }
+    }
+    printf("\n%d\n%d\n", zeros, ones);
+}
 
-    return 0;
+void intBitit(int x) {
+    int i, k, ones, zeros, bits;
+    ones = 0;
+    zeros = 0;
+    bits = 32;
+
+    printf("%d\n", x);
+    for (i = bits - 1; i >= 0; i--) {
+        k = x >> i;
+        if (k & 1) {
+            printf("1");
+            ones++;
+        }
+        else {
+            printf("0");
+            zeros++;
+        }
+    }
+    printf("\n%d\n%d\n", zeros, ones);
+}
+
+void longBitit(long int x) {
+    int i, k, ones, zeros, bits;
+    ones = 0;
+    zeros = 0;
+    bits = 64;
+
+    printf("%ld\n", x);
+    for (i = bits - 1; i >= 0; i--) {
+        k = x >> i;
+        if (k & 1) {
+            printf("1");
+            ones++;
+        }
+        else {
+            printf("0");
+            zeros++;
+        }
+    }
+    printf("\n%d\n%d\n", zeros, ones);
 }
